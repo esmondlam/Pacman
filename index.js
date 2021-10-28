31dce66a7a0432d08be907cbca203a26d9dc1676
Esmond Lam <esmondlam0923@gmail.com>
2021-10-28 18:43:55 +0800

    add control function

--- index.js
+++ index.js
@@ -64,3 +64,46 @@ function createBoard() {
 }
 
 createBoard();
+
+//starting position of pacman
+let pacmanCurrentIndex = 490;
+squares[pacmanCurrentIndex].classList.add('pacman');
+
+//down - 40
+//up - 38
+//left - 37
+//right - 39
+console.log(pacmanCurrentIndex)
+
+function control(e) {
+    squares[pacmanCurrentIndex].classList.remove('pacman')
+
+    switch(e.keyCode) {
+        case 40:
+        if (
+            !squares[pacmanCurrentIndex + width].classList.contains('wall') &&
+            pacmanCurrentIndex + width < width * width) pacmanCurrentIndex += width; 
+        break;
+
+        case 39:
+        if (
+            !squares[pacmanCurrentIndex + 1].classList.contains('wall') &&
+            pacmanCurrentIndex % width < width - 1) pacmanCurrentIndex += 1;; 
+        break;
+
+        case 38:
+        if (
+            !squares[pacmanCurrentIndex - width].classList.contains('wall') &&
+            pacmanCurrentIndex - width >= 0) pacmanCurrentIndex -= width; 
+        break;
+
+        case 37:
+        if (
+            !squares[pacmanCurrentIndex - 1].classList.contains('wall') &&
+            pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -= 1;  
+        break;
+    }
+    squares[pacmanCurrentIndex].classList.add('pacman')
+}
+
+document.addEventListener('keyup', control);
